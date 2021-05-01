b9ccb88a312eb9d8fe0d8f50fde985f78fb35c11
Yanchen Ma <yanchenm@gmail.com>
2021-05-01 03:51:27 -0600

    fix sql formatting

--- api/db/migrations/V1__init_schema.sql
+++ api/db/migrations/V1__init_schema.sql
@@ -1,49 +1,28 @@
 CREATE TABLE IF NOT EXISTS Users
 (
-    email
-    TEXT
-    PRIMARY
-    KEY,
-    name
-    TEXT,
-    password
-    VARCHAR
-(
-    60
-),
+    email      TEXT PRIMARY KEY,
+    name       TEXT,
+    password   VARCHAR(60),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+);
 
 CREATE TABLE IF NOT EXISTS Photos
 (
-    id CHAR
-(
-    27
-) PRIMARY KEY,
-    username TEXT REFERENCES Users
-(
-    email
-) ON DELETE CASCADE,
-    filename TEXT NOT NULL,
-    key TEXT NOT NULL,
-    thumbnail TEXT NOT NULL,
+    id          CHAR(27) PRIMARY KEY,
+    username    TEXT REFERENCES Users (email) ON DELETE CASCADE,
+    filename    TEXT NOT NULL,
+    key         TEXT NOT NULL,
+    thumbnail   TEXT NOT NULL,
     uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    filetype TEXT NOT NULL,
-    height INT NOT NULL,
-    width INT NOT NULL,
-    size INT NOT NULL
-    );
+    filetype    TEXT NOT NULL,
+    height      INT NOT NULL,
+    width       INT NOT NULL,
+    size        INT NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS Auth
 (
-    email TEXT REFERENCES Users
-(
-    email
-) ON DELETE CASCADE ,
+    email TEXT REFERENCES Users (email) ON DELETE CASCADE ,
     token TEXT UNIQUE,
-    PRIMARY KEY
-(
-    email,
-    token
-)
-    );
+    PRIMARY KEY (email, token)
+);
