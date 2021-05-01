db981f9d5511c4bff513e96796392c77caa84b1d
Yanchen Ma <yanchenm@gmail.com>
2021-05-01 03:50:42 -0600

    begin python serverless migration

--- api/db/migrations/V1__init_schema.sql
+++ api/db/migrations/V1__init_schema.sql
@@ -1,33 +1,49 @@
 CREATE TABLE IF NOT EXISTS Users
 (
-    email      TEXT PRIMARY KEY,
-    name       TEXT,
-    password   VARCHAR(60),
+    email
+    TEXT
+    PRIMARY
+    KEY,
+    name
+    TEXT,
+    password
+    VARCHAR
+(
+    60
+),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    );
 
 CREATE TABLE IF NOT EXISTS Photos
 (
-    id          CHAR(27) PRIMARY KEY,
-    username    TEXT REFERENCES Users (email) ON DELETE CASCADE,
-    filename    TEXT NOT NULL,
-    key         TEXT NOT NULL,
-    thumbnail   TEXT NOT NULL,
-    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS Details
+    id CHAR
 (
-    id           CHAR(27) PRIMARY KEY REFERENCES Photos (id) ON DELETE CASCADE,
-    filetype    TEXT  NOT NULL,
-    height       INT   NOT NULL,
-    width        INT   NOT NULL,
-    size         FLOAT NOT NULL
-);
+    27
+) PRIMARY KEY,
+    username TEXT REFERENCES Users
+(
+    email
+) ON DELETE CASCADE,
+    filename TEXT NOT NULL,
+    key TEXT NOT NULL,
+    thumbnail TEXT NOT NULL,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    filetype TEXT NOT NULL,
+    height INT NOT NULL,
+    width INT NOT NULL,
+    size INT NOT NULL
+    );
 
 CREATE TABLE IF NOT EXISTS Auth
 (
-    email TEXT REFERENCES Users (email) ON DELETE CASCADE ,
+    email TEXT REFERENCES Users
+(
+    email
+) ON DELETE CASCADE ,
     token TEXT UNIQUE,
-    PRIMARY KEY (email, token)
-);
+    PRIMARY KEY
+(
+    email,
+    token
+)
+    );
--- api/models/auth.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type RefreshToken struct {
-	Email string `json:"email"`
-	Token string `json:"token"`
-}
